0a520383fe0f460bd4468909ee0e565d91f0b7c2
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-25 23:28:34 +0100

    feat: :sparkles: Update method updatePrice and add method applyDiscount

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ var Category = require('./src/domain/Category/category');
 
 // Create category
 var category = Object.create(Category).init('premium', 50);
-
+var seatCupra = Object.create(Vehicle).init('seat', 'cupra', 'grey', 50, category);
 // Create vehicle
 console.log('Vehicle prepared !');
-console.log('Vehicle: ', Object.create(Vehicle).init('seat', 'cupra', 'grey', category));
+console.log(`Vehicle: ${seatCupra}`);
+console.log(seatCupra.getName());
+console.log(seatCupra.getOriginalPrice());
 
+console.log('Número props: ' + Object.keys(seatCupra).length);
 
--- src/domain/Vehicle/vehicle.js
+++ src/domain/Vehicle/vehicle.js
@@ -1,39 +1,76 @@
 var Vehicle = {
-    init: function(
+    init: function (
         brand, model, color, price, category) {
         this.brand = brand;
         this.model = model;
         this.color = color;
-        this.price = price
+        this.price = price;
         this.category = category;
+        //! No la qiuiero inicializar ahora, sino cuando reciba el precio de bd, así me ahorro un campo en la bd !!!
+        Object.defineProperty(this, "originalPrice", {
+            value: price,
+            writeable: false,
+            enumerable: false,
+        });
+        //? Donde explico que la regla de mi negocio no permite bajar el valor de su coche una vez llegué al 30% de su valor inicial ???
+        Object.defineProperty(this, "minPrice", {
+            value: price * 0.3,
+            writeable: false,
+            enumerable: false,
+        });
         return this;
     },
     // GETTERS
-    getBrand: function() {
+    getBrand: function () {
         return `Vehicle brand: ${this.brand}`;
     },
-    getModel: function() {
+    getModel: function () {
         return `Vehicle model: ${this.model}`;
     },
-    getName: function() {
+    getName: function () {
         return `Vehicle name: ${this.brand} ${this.model}`;
     },
-    getColor: function() {
+    getColor: function () {
         return `Vehicle color: ${this.color}`;
     },
-    getCategory: function() {
+    getCategory: function () {
         return this.category;
     },
-    getPrice: function() {
+    getPrice: function () {
         return `Vehicle price: ${this.price}`;
     },
-    // DATA MANIPULATION
-    //? Porque si accedo al método getDiscountTax() me devuelve NaN ???
-    updatePrice: function() {
-        this.price = Math.floor((100 * this.category.discountTax) / this.price);
+    getOriginalPrice: function () {
+        return 'Vehicle original price: ' + this.originalPrice;
     },
-    getUpdatedPrice: function() {
+    getErrorMinPrice: function () {
+        return 'Price can not go under minimum';
+    },
+    // DATA MANIPULATION
+    /**
+     * TODO: Revisar esto con atención
+     //! Las validaciones se deberían hacer en el service y el objeto debería encapsular una lógica simple que solo sea llamada si se cumple la condición ??? Los if-else deberían ir en el service ?
+     */
+    //? Porque si accedo al método getDiscountTax() me devuelve NaN ???
+    updatePrice: function () {
+        //TODO: aplicar prog. defensiva, chequear que el precio no se puede descontar si es inferior al precioOriginal
+        // Siempre el descuento será de un 5% en todos los vehiculos 
+        if (this.price > this.minPrice) {
+            this.price -= this.price * 0.1; 
+        }
+        else {
+            return this.getErrorMinPrice();
+        }
+    },
+    getDiscountedPrice: function () {
         return `Discount applied successfully! \nPrice: ${this.price}`;
+    },
+    applyDiscount: function () {
+        if (this.price > this.minPrice) {
+            this.price = Math.floor((100 * this.category.discountTax) / this.price);
+        }
+        else {
+            return this.getErrorMinPrice();
+        }
     }
 };
 
