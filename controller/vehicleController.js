71437734893b114aad186a6ca866b34784e2c662
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-27 19:10:55 +0100

    feat: :sparkles: Add first endpoint to get stock calling service

--- controller/vehicleController.js
+++ controller/vehicleController.js
@@ -1,4 +1,6 @@
 const vehicle = require('./../src/domain/vehicle/vehicle');
+const stockResource = require('../resource/stockResource');
+const stockService = require('../service/service');
 
 var vehicleAPI = (function singleVehicleController() {
 
@@ -7,8 +9,16 @@ var vehicleAPI = (function singleVehicleController() {
     //     res.status(200).type('json').json(box);
     // }
 
-    const vehicleList = function(req, res) {
-        res.status(200).send('NOT IMPLEMENTED: Vehicle list');
+    //? El modulo resource nos lo podemos ahorrar ???
+    //* Ya que en principio el la logica de Ollivaners habiamos creado uno ya que el controller actuaba como el router y el resource de Ollivanders actuaria como el controller de node
+
+    // const vehicleList = function(req, res) {
+    //     res.status(200).type('json').json(stockResource.stockResource.getStock());
+    //     // res.status(200).send('NOT IMPLEMENTED: Vehicle list');
+    // }
+
+        const vehicleList = function(req, res) {
+        res.status(200).type('json').json(stockService.serviceAPI.getStockList());
     }
 
     const vehicleFindOne = function(req, res) {
