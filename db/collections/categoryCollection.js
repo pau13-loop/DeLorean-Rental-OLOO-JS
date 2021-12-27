d4251e0b81041ee28536a5af2de51210a4c91e35
pau13-loop <pllinas@cifpfbmoll.eu>
2021-12-27 17:49:21 +0100

    feat: :sparkles: Add data collections

---
+++ db/collections/categoryCollection.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        name: "common",
+        discountTax: 60
+    },
+    {
+        name: "premium",
+        discountTax: 20
+    },
+    {
+        name: "classic",
+        discountTax: 40
+    }
+];
