7c2b15198f821beaf112e12bc1a21606f8173e8b
pau13-loop <pllinas@cifpfbmoll.eu>
2021-12-27 17:49:45 +0100

    feat: :sparkles: Create models

---
+++ models/category.js
@@ -0,0 +1,14 @@
+var Schema = mongoose.Schema;
+
+var CategoryModelSchema = new Schema ({
+    name: {
+        type: String, 
+        required: true
+    },
+    discountTax: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('CategoryModel', CategoryModelSchema);
---
+++ models/vehicle.js
@@ -0,0 +1,38 @@
+const Category = require("../src/domain/category/category");
+
+var Schema = mongoose.Schema;
+
+var VehicleModelSchema = new Schema({
+    model: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: Schema.Types.ObjectId, 
+        enum: ['premium', 'common', 'classic'],
+        ref: Category.name
+    },
+    passengers: {
+        type: Number, 
+        required: true
+    },
+    year: {
+        type: Number, 
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: true
+    }
+});
+
+module.exports = mongoose.mdodel('VehicleModel', VehicleModelSchema);
+
