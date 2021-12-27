cd770e062ff3d4e21bed882d8f60afe978952674
pau13-loop <pllinas@cifpfbmoll.eu>
2021-12-27 21:48:56 +0100

    feat: :sparkles: Keep old mock stable version

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-// EXPRESS DEFAULT // 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+ //* MongoDB CONNECTION *//
 
-//* DB CONNECTION *//
 
-var mongoConfig = require('./db/mongoConfig');
-mongoConfig.connect();
-// connection es el constructor de la conexion
-var db = mongoConfig.mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// DEFAULT ROUTERS //
+//* ROUTERS *//
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//* DOMAIN ROUTERS *//
-// var stockRouter = require('./routes/stock');
-var stockRouter = require('./routes/stockDB');
-//! Need to implement
-// var categoryRouter = require('./routes/category');
+var stockRouter = require('./routes/stock');
 
+//* App setup *//
 
-//* APP SETUP *//
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,14 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 //* MIDELWARE *//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stock', stockRouter);
 
 
-// ERROR HANDLING //
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
