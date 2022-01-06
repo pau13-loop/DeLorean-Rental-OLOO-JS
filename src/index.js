88d6350fa055e401b5a4acbcddd842c6d84f8802
pau13-loop <pllinas@cifpfbmoll.eu>
2022-01-06 15:16:09 +0100

    feat: :sparkles: Improve displaying of views and add an explanation about project

--- src/index.js
+++ src/index.js
@@ -24,14 +24,14 @@ var app = express();
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 //* MIDELWARE *//
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcom to Rent-a-car OLOO ! Aquí estoy, buscame después' });
+  res.render('index', { title: 'Welcome to Rent-a-car OLOO !' });
 });
 
 module.exports = router;
