01b47ff240e1db6790bf4d765aaccc39d12e17a2
pau13-loop <pllinas@cifpfbmoll.eu>
2022-01-08 15:00:46 +0100

    feat: :sparkles: Add customers collection

---
+++ src/db/collections/customersCollection.js
@@ -0,0 +1,34 @@
+const ObjectId = require('mongodb').ObjectId;
+
+module.exports = [
+    {
+        name: 'Marty McFly',
+        birthDate: '1968/06/12',
+        dniNumber: 17608824,
+        dniLetter: 'R',
+    },
+    {
+        name: 'Niki Lauda',
+        birthDate: '1949/02/22',
+        dniNumber: 32361387,
+        dniLetter: L,
+    },
+    {
+        name: 'Lewis Hamilton',
+        birthDate: '1985/01/07',
+        dniNumber: 45462541,
+        dniLetter: M,
+    },
+    {
+        name: 'Travis Bickle',
+        birthDate: '1976/05/13',
+        dniNumber: 68096750,
+        dniLetter: Y,
+    },
+    {
+        name: 'Baby',
+        birthDate: '1994/03/14',
+        dniNumber: 63453896,
+        dniLetter: 'R',
+    }
+];
